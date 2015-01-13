064570ae6b9e20d36f3463fd5bd4786eca29279b
Charles Aylward <charles.aylward@gmail.com>
2015-01-13 01:23:00 -0800

    schema: Add base StringType for schema types.

---
+++ src/appc/schema/common.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#include "appc/util/Try.h"
+
+#include "3rdparty/nlohmann/json.h"
+
+
+namespace appc {
+namespace schema {
+
+using Json = nlohmann::json;
+
+
+struct Status {
+  const bool successful;
+  const std::string message;
+
+  explicit Status(const bool successful,
+                  const std::string& message)
+  : successful(successful),
+    message(message) {}
+
+  operator bool() const {
+    return successful;
+  }
+};
+
+
+Status Valid() {
+  return Status(true, "");
+}
+
+
+Status Invalid(const std::string& message) {
+  return Status(false, message);
+}
+
+
+template<typename T>
+struct Type {
+  virtual ~Type() = default;
+  virtual Status validate() = 0;
+};
+
+
+template<typename T>
+struct StringType : Type<T> {
+  const std::string value;
+
+  explicit StringType<T>(const std::string value)
+  : value(value) {}
+
+  bool operator==(const StringType<T>& other) const {
+    return value == other.value;
+  }
+
+  bool operator!=(const StringType<T>& other) const {
+    return !(*this == other);
+  }
+
+  operator std::string() const {
+    return value;
+  }
+
+  static Try<T> from_json(const Json& json) {
+    return Success(T(json.get<std::string>()));
+  }
+
+  static Try<T> from_json(const std::nullptr_t null) {
+    return Failure<T>("Expected JSON string type");
+  }
+
+  static Json to_json(const T& string_type) {
+    return Json { string_type.value };
+  }
+};
+
+
+} // namespace schema
+} // namespace appc
---
+++ tests/unit/appc/schema/test_common.cpp
@@ -0,0 +1,51 @@
+#include "gtest/gtest.h"
+
+#include "appc/schema/common.h"
+
+using namespace appc::schema;
+
+
+TEST(Status, valid_is_true) {
+  ASSERT_TRUE(Valid().successful);
+  ASSERT_TRUE(Valid());
+}
+
+TEST(Status, invalid_is_false) {
+  ASSERT_FALSE(Invalid("invalid").successful);
+  ASSERT_FALSE(Invalid("invalid"));
+}
+
+
+struct SomeStringType : StringType<SomeStringType> {
+  explicit SomeStringType(const std::string& val)
+  : StringType<SomeStringType>(val) {}
+  Status validate() {
+    return Valid();
+  }
+};
+
+TEST(StringType, is_implicit_string) {
+  std::string some_string { SomeStringType("a string") };
+  ASSERT_EQ("a string", some_string);
+}
+
+TEST(StringType, same_values_equate) {
+  ASSERT_EQ(SomeStringType("a string"), SomeStringType("a string"));
+}
+
+TEST(StringType, different_values_dont) {
+  ASSERT_NE(SomeStringType("x string"), SomeStringType("y string"));
+}
+
+TEST(StringType, from_json) {
+  const Json json = "A String";
+  Try<SomeStringType> a_try = SomeStringType::from_json(json);
+  ASSERT_TRUE(a_try);
+  ASSERT_EQ(SomeStringType("A String"), from_success(a_try));
+}
+
+TEST(StringType, to_json) {
+  const Json expected_json { "A String" };
+  const Json json = SomeStringType::to_json(SomeStringType("A String"));
+  ASSERT_EQ(expected_json, json);
+}

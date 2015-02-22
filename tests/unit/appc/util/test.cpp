4f16ca62f0905d9ed6cde3ac1cda2034efcadfb5
Charles Aylward <charles.aylward@gmail.com>
2015-02-22 00:31:10 -0800

    tests: Break appc/util tests into individual comp. units.

--- tests/unit/appc/util/test.cpp
+++
@@ -1,6 +0,0 @@
-#include "gtest/gtest.h"
-
-#include "test_option.h"
-#include "test_try.h"
-#include "test_try_option.h"
-
--- tests/unit/appc/util/test_try_option.cpp
+++ tests/unit/appc/util/test_try_option.cpp
@@ -1,7 +1,6 @@
-#pragma once
-
 #include <exception>
 
+#include "gtest/gtest.h"
 #include "appc/util/try_option.h"
 
 

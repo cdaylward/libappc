80cfa4a0bcdcc7d1fb94077273fe26d8769e1b13
Charles Aylward <charles.aylward@gmail.com>
2015-02-21 18:02:04 -0800

    schema: provide conversion from EventHandlers to map.
    - Add validator that checks for duplicates. Needs test.

--- src/appc/schema/event_handlers.h
+++ src/appc/schema/event_handlers.h
@@ -17,6 +17,8 @@
 
 #pragma once
 
+#include <set>
+
 #include "appc/schema/ac_name.h"
 #include "appc/schema/common.h"
 #include "appc/schema/exec.h"
@@ -31,6 +33,10 @@ namespace schema {
 struct EventName : ACName<EventName> {
   explicit EventName(const std::string& name)
   : ACName<EventName>(name) {}
+
+  bool operator<(const EventName& rhs) const {
+    return value < rhs.value;
+  };
 };
 
 
@@ -64,6 +70,28 @@ struct EventHandler : Type<EventHandler> {
 struct EventHandlers : ArrayType<EventHandlers, EventHandler> {
   explicit EventHandlers(const std::vector<EventHandler>& args)
   : ArrayType<EventHandlers, EventHandler>(args) {}
+
+  operator std::map<EventName, Exec>() const {
+    std::map<EventName, Exec> map;
+    for (auto& handler : array) {
+      map.emplace(handler.name, handler.exec);
+    }
+    return map;
+  }
+
+  virtual Status validate() const {
+    std::set<EventName> seen;
+    for (const auto& handler : this->array) {
+      if (seen.find(handler.name) != seen.end()) {
+        return Invalid(std::string{"event_handlers has duplicate definition "} + handler.name.value);
+      }
+      auto valid = handler.validate();
+      if (!valid) {
+        return valid;
+      }
+    }
+    return Valid();
+  }
 };
 
 

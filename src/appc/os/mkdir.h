ebf01213a3183b32749dad3b9f88eaf690a72d35
Charles Aylward <charles.aylward@gmail.com>
2015-02-28 20:26:16 -0800

    os: Update mkdir to use syscalls, not call system.

--- src/appc/os/mkdir.h
+++ src/appc/os/mkdir.h
@@ -17,13 +17,12 @@
 
 #pragma once
 
-#include <cerrno>
-#include <cstdlib>
-#include <cstring>
-#include <iostream>
+#include <sys/stat.h>
 #include <sys/types.h>
 
 #include "3rdparty/cdaylward/pathname.h"
+
+#include "appc/os/errno.h"
 #include "appc/util/status.h"
 
 
@@ -32,17 +31,22 @@ namespace os {
 
 
 inline Status mkdir(const std::string& dir, const mode_t mode, const bool create_parents = false) {
-  if (create_parents && !pathname::is_absolute(dir)) {
-    return Error(std::string{"Will not create parents for relative path: "} + dir);
-  }
-  // FIXME, use syscall, plumb mode
-  std::string mkdir_command = "mkdir ";
   if (create_parents) {
-    mkdir_command += "-p ";
-  }
-  mkdir_command += "-- " + dir;
-  if (system(mkdir_command.c_str()) != 0) {
-    return Error(std::string{"Could not create "} + dir + ": " + strerror(errno));
+    if (!pathname::is_absolute(dir)) {
+      return Error(std::string{"Will not create parents for relative path: "} + dir);
+    }
+    std::string current{"/"};
+    for (const auto& entry : pathname::split(dir)) {
+      if (entry == "/") continue;
+      current = pathname::join(current, entry);
+      struct stat dummy;
+      if (stat(current.c_str(), &dummy) == 0) continue;
+      const int result = ::mkdir(current.c_str(), mode);
+      if (result == -1) return Errno(dir + " could not be created", errno);
+    }
+  } else {
+    const int result = ::mkdir(dir.c_str(), mode);
+    if (result == -1) return Errno(dir + " could not be created", errno);
   }
   return Success();
 }
